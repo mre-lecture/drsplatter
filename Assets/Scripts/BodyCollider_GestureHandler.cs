3f5cf846ae1d5ae831d7591ccc13809ebecf5b2a
1321302 Kevin Schorpp <1321302@stud.hs-mannheim.de>
2017-05-20 21:20:27 +0200

    Added Capsule Colliders ( which will later be invisible ) to represent interactible Body Parts. Added BodyCollider Script Test.
    ToDo: Make the Gaze / Gesture to Object Interaction work.

---
+++ Assets/Scripts/BodyCollider_GestureHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyCollider_GestureHandler : MonoBehaviour {
+
+    private bool isActive = false;
+    private bool isRed = false;
+
+        void Update()
+        {
+
+            if (isActive)
+            {
+            if (!isRed)
+            {
+                Renderer rend = this.GetComponent<Renderer>();
+                rend.material.shader = Shader.Find("Specular");
+                rend.material.SetColor("_SpecColor", Color.red);
+                isRed = !isRed;
+            }
+            else
+            {
+                Renderer rend = this.GetComponent<Renderer>();
+                rend.material.shader = Shader.Find("Specular");
+                rend.material.SetColor("_SpecColor", Color.white);
+                isRed = !isRed;
+            }
+                //this.transform.Rotate(0, 1, 0);
+            }
+
+        }
+
+        void OnAirTapped()
+        {
+            isActive = !isActive;
+        }
+
+}
