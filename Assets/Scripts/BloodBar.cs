d42784449c7f0f2699599313bfe56bfbb7a0180a
1321302 Kevin Schorpp <1321302@stud.hs-mannheim.de>
2017-05-22 10:05:22 +0200

    Modified BloodBar UI Added BloodBar Script (not linked to anything yet)

---
+++ Assets/Scripts/BloodBar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BloodBar : MonoBehaviour {
+
+    public Image currentBloodBar;
+    public Text ratioText;
+
+    private float bloodLevel = 150;
+    private float maxBloodLevel = 150;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void UpdateBloodBar()
+    {
+        float ratio = bloodLevel / maxBloodLevel;
+        currentBloodBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        ratioText.text = (ratio * 100).ToString("0") + '%';
+    }
+
+    private void TakeDamage(float damage)
+    {
+        bloodLevel -= damage;
+        if(bloodLevel < 0)
+        {
+            bloodLevel = 0;
+        }
+        UpdateBloodBar();
+    }
+
+    private void HealBloodLoss(float heal)
+    {
+        bloodLevel += heal;
+        if (bloodLevel > maxBloodLevel)
+        {
+            bloodLevel = maxBloodLevel;
+        }
+        UpdateBloodBar();
+    }
+}
