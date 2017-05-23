76ff92d1f7416241dfd813431df4a7d58e8cc960
1321302 Kevin Schorpp <1321302@stud.hs-mannheim.de>
2017-05-23 11:34:51 +0200

    Updated BodyCollider_GestureHandler to correspond with new HoloToolkit Update and use InputHandlers instead of TapEvents.

--- Assets/Scripts/BodyCollider_GestureHandler.cs
+++ Assets/Scripts/BodyCollider_GestureHandler.cs
@@ -1,39 +1,40 @@
-﻿using System.Collections;
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BodyCollider_GestureHandler : MonoBehaviour {
+public class BodyCollider_GestureHandler : MonoBehaviour, IInputClickHandler, IInputHandler
+{
 
     private bool isActive = false;
     private bool isRed = false;
 
-        void Update()
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        // AirTap code goes here
+        if (!isRed)
+        {
+            Renderer rend = this.GetComponent<Renderer>();
+            rend.material.shader = Shader.Find("Specular");
+            rend.material.SetColor("_SpecColor", Color.red);
+            isRed = !isRed;
+        }
+        else
+        {
+            Renderer rend = this.GetComponent<Renderer>();
+            rend.material.shader = Shader.Find("Specular");
+            rend.material.SetColor("_SpecColor", Color.white);
+            isRed = !isRed;
+        }
+}
+
+    public void OnInputDown(InputEventData eventData)
+    { }
+    public void OnInputUp(InputEventData eventData)
+    { }
+
+    void Update()
         {
 
-            if (isActive)
-            {
-            if (!isRed)
-            {
-                Renderer rend = this.GetComponent<Renderer>();
-                rend.material.shader = Shader.Find("Specular");
-                rend.material.SetColor("_SpecColor", Color.red);
-                isRed = !isRed;
-            }
-            else
-            {
-                Renderer rend = this.GetComponent<Renderer>();
-                rend.material.shader = Shader.Find("Specular");
-                rend.material.SetColor("_SpecColor", Color.white);
-                isRed = !isRed;
-            }
-                //this.transform.Rotate(0, 1, 0);
-            }
-
         }
-
-        void OnAirTapped()
-        {
-            isActive = !isActive;
-        }
-
 }
